19f01e82da9b99a4bc55af714ab831972e7e20b7
Lucas de Melo da Silva <lu_lucasmelo@hotmail.com>
2018-05-09 15:38:55 -0300

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,5 @@
--- descobrir todos os salarios da matricula 86
+
+ -----------------------------------------------------------------------------------------------
 
 select * from (
 				select sub.Idf_Empregado, count(*) as rep 
@@ -10,8 +11,9 @@ select * from (
 				where Idf_Empregado between 3900 and 3910
 				group by Idf_Empregado
 				) as t
-where t.rep between 30 and 40
-
+where t.rep between 30 and 40 
+ 
+ -----------------------------------------------------------------------------------------------
 
 select sub.Idf_Empregado, count(*) as rep 
 from (
@@ -22,6 +24,8 @@ from (
 where Idf_Empregado between 3900 and 3910
 group by Idf_Empregado
 having count(*) between 30 and 40
+
+ -----------------------------------------------------------------------------------------------
 
 SELECT Idf_Salario as ID, Sig_Moeda as Moeda, Num_Matricula as NumeroMatricula, 
 Dta_Fim_Vigencia as DataFimVigencia, Vlr_Salario as ValorSalario
@@ -34,6 +38,8 @@ and Idf_Salario between 20000 and 24000
 --group by Idf_Salario
 ORDER BY ID
 
+ -----------------------------------------------------------------------------------------------
+
 -- descobrir todos os contratos da matricula 86
 select * --TAB_Empregado.Idf_Empregado, TAB_Empregado.Num_Matricula, TAB_Empregado_Contrato.Idf_Contrato, TAB_Empregado_Contrato.Dta_Cadastro, TAB_Salario.Vlr_Salario
 from TAB_Empregado
@@ -41,40 +47,47 @@ join tab_Empregado_Contrato on TAB_Empregado.Idf_Empregado = tab_empregado_Contr
 join TAB_Salario on TAB_Empregado.Idf_Empregado = TAB_Salario.Idf_Empregado
 where Num_Matricula = 86;
 
+ -----------------------------------------------------------------------------------------------
+
 select *--TAB_Empregado.Idf_Empregado, TAB_Empregado.Num_Matricula, TAB_Empregado_Contrato.Idf_Contrato, TAB_Empregado_Contrato.Dta_Cadastro, TAB_Salario.Vlr_Salario
 from TAB_Empregado
 left join tab_Empregado_Contrato on tab_empregado_Contrato.Idf_Empregado = TAB_Empregado.Idf_Empregado 
 left join TAB_Salario on TAB_Salario.Idf_Empregado = TAB_Empregado.Idf_Empregado
 where Num_Matricula = 86; 
 
+ -----------------------------------------------------------------------------------------------
+
 select * from TAB_Salario
 where Idf_Empregado = 3833
-
 
 select * from tab_Empregado_Contrato where Idf_Empregado = 3833
 
 select * from TAB_Empregado where Num_Matricula = 86;
 
+ -----------------------------------------------------------------------------------------------
 
-CREATE VIEW ver As
-SELECT Vlr_Salario, Sig_Moeda
-FROM TAB_Salario
-WHERE Idf_Salario = 12796
+select MAX(Idf_salario) as MaiorId 
+from TAB_Salario
 
+ -----------------------------------------------------------------------------------------------
 
 select * from TAB_Salario
+WHERE Idf_Salario = 12796;
 
+--begin tran 
+UPDATE TAB_Salario
+SET Vlr_Salario = 0.15
+WHERE Idf_Salario = 12796;
+--commit
+--rollback
 
+delete from TAB_Salario
+WHERE Idf_Salario = 12796;
 
-
-
-
-
+ -----------------------------------------------------------------------------------------------
 
 -- DESCOBRIR O RELACIONAMENTO DAS TABELAS 
 Sp_HELP 'TAB_Empregado'
-
-select * from TAB_Salario
 
 
 
